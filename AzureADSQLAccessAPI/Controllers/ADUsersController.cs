f7ba40d837064013bd69f85a88bdc29e7872d412
David RodriguezAldana <david.rodriguezaldana@nttdata.com>
2021-04-19 10:58:22 +0200

    Creation

---
+++ AzureADSQLAccessAPI/Controllers/ADUsersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Azure.Core;
+using Azure.Identity;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Logging;
+
+namespace AzureADSQLAccessAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ADUsersController : ControllerBase
+    {       
+
+        [HttpGet]
+        public async Task<List<string>> Get()
+        {          
+
+            var connectionStringBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = "tcp:gwhhserver2.database.windows.net,1433",
+                InitialCatalog = "gwhhdb",
+                TrustServerCertificate = false,
+                Encrypt = true
+            };
+
+            await using var sqlConnection = new SqlConnection(connectionStringBuilder.ConnectionString)
+            {
+                AccessToken = await GetAzureSqlAccessToken()
+            };
+
+
+
+            return new List<string>()
+            {
+                "Hello",
+                "World"
+            };
+        }
+
+        private static async Task<string> GetAzureSqlAccessToken()
+        {
+            
+            var tokenRequestContext = new TokenRequestContext(new[] { "https://database.windows.net//.default" });
+            var tokenRequestResult = await new DefaultAzureCredential().GetTokenAsync(tokenRequestContext);
+
+            return tokenRequestResult.Token;
+        }
+    }
+}
---
+++ AzureADUserApi/Controllers/ADUsersController.cs
@@ -0,0 +1,56 @@
+﻿using Azure.Core;
+using Azure.Identity;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureADUserApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ADUsersController : ControllerBase
+    { 
+
+
+        [HttpGet]
+        public async Task<List<string>> Get()
+        {
+
+            var connectionStringBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = "tcp:gwhhserver2.database.windows.net,1433",
+                InitialCatalog = "gwhhdb",
+                TrustServerCertificate = false,
+                Encrypt = true
+            };
+
+            await using var sqlConnection = new SqlConnection(connectionStringBuilder.ConnectionString)
+            {
+                AccessToken = await GetAzureSqlAccessToken()
+            };
+
+
+
+            return new List<string>()
+            {
+                "Hello",
+                "World"
+            };
+        }
+
+        private static async Task<string> GetAzureSqlAccessToken()
+        {
+
+            var tokenRequestContext = new TokenRequestContext(new[] { "https://database.windows.net" });
+            var tokenRequestResult = await new DefaultAzureCredential(new DefaultAzureCredentialOptions { ExcludeVisualStudioCodeCredential = true }).GetTokenAsync(tokenRequestContext);
+
+            return tokenRequestResult.Token;
+        }
+    }
+}
